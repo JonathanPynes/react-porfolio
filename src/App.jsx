13b5c23705ee6ce877a1ff8a6694ea3bf9a4b304
Jonathan Pynes <jonpynes@gmail.com>
2021-07-27 16:06:21 -0500

    added 3djs and background vanta.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,80 @@
-import logo from "./logo.svg";
 import BackgroundHeader from "./Asset/background-header.jpg";
 import LinkedIn from "./Asset/linkedin_1.svg";
 import Sms from "./Asset/sms_1.svg";
 import Github from "./Asset/github_1.svg";
 import styled from "styled-components";
+import React, { useState, useEffect, useRef } from "react";
+import BIRDS from "vanta/dist/vanta.birds.min";
+
+const MyComponent = (props) => {
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const myRef = useRef(null);
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        BIRDS({
+          el: myRef.current,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 450.0,
+          minWidth: 200.0,
+          scale: 1.0,
+          scaleMobile: 1.0,
+          backgroundColor: 0x120820,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+  return <div ref={myRef}>Foreground content goes here</div>;
+};
 
 function App() {
   return (
-    <HeaderBackgroundCSS>
-      <HeaderContainerCSS>
-        <section>
-          {/* Social connect logos */}
-          <IconAlignmentCSS>
-            <IconForEachCSS>
-              <a href="https://www.linkedin.com/in/jonpynes" target="_blank">
-                <img src={LinkedIn} alt="LinkedIn" />
-              </a>
-            </IconForEachCSS>
-            <IconForEachCSS>
-              <a href="sms:4027304258">
-                <img src={Sms} alt="Text SMS" class="contact-logo-sms-image" />
-              </a>
-            </IconForEachCSS>
-            <IconForEachCSS>
-              <a href="https://github.com/JonathanPynes" target="_blank">
-                <img
-                  src={Github}
-                  alt="GitHub"
-                  class="contact-logo-github-image"
-                />
-              </a>
-            </IconForEachCSS>
-          </IconAlignmentCSS>
-        </section>
-      </HeaderContainerCSS>
-    </HeaderBackgroundCSS>
+    <>
+      <HeaderBackgroundCSS>
+        <HeaderContainerCSS>
+          <section>
+            {/* Social connect logos */}
+            <IconAlignmentCSS>
+              <IconForEachCSS>
+                <a href="https://www.linkedin.com/in/jonpynes" target="_blank">
+                  <img src={LinkedIn} alt="LinkedIn" />
+                </a>
+              </IconForEachCSS>
+              <IconForEachCSS>
+                <a href="sms:4027304258">
+                  <img
+                    src={Sms}
+                    alt="Text SMS"
+                    class="contact-logo-sms-image"
+                  />
+                </a>
+              </IconForEachCSS>
+              <IconForEachCSS>
+                <a href="https://github.com/JonathanPynes" target="_blank">
+                  <img
+                    src={Github}
+                    alt="GitHub"
+                    class="contact-logo-github-image"
+                  />
+                </a>
+              </IconForEachCSS>
+            </IconAlignmentCSS>
+          </section>
+        </HeaderContainerCSS>
+      </HeaderBackgroundCSS>
+      <MyComponent></MyComponent>
+    </>
   );
 }
+
+const MyComponentCSS = styled.section`
+  height: 100vh;
+`;
 
 const HeaderBackgroundCSS = styled.section`
   background-image: url(${BackgroundHeader});
@@ -58,7 +96,8 @@ const IconAlignmentCSS = styled.section`
   flex-direction: row;
   margin: 0.5rem;
   align-items: flex-end;
-  position: absolute;
+  position: fixed;
+  z-index: 99;
   top: 0;
   right: 0;
 `;
