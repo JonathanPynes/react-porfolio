c882aee9523bd619771fab683f9ffd192bd18ae0
Jonathan Pynes <jon.pynes@shineforth.co>
2022-04-12 21:11:52 -0500

    got rid of dead code

--- src/Components/useInterval.jsx
+++ src/Components/useInterval.jsx
@@ -1,16 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
 
-// function Counter() {
-//   let [count, setCount] = useState(0);
-
-//   useInterval(() => {
-//     // Your custom logic here
-//     setCount(count + 1);
-//   }, 1000);
-
-//   return <h1>{count}</h1>;
-// }
-
 export function useInterval(callback, delay) {
   const savedCallback = useRef();
 
